64be32a009fd18eb4181263f68db5a73854fb21d
j4jay <rawal.jay@gmail.com>
2025-04-11 21:20:28 +0530

    Create code1.js

---
+++ code1.js
@@ -0,0 +1,5 @@
+let bookData = [
+    { "title": "The Lost Horizon", "author": "James Hilton", "year": 1933, "genre": "Adventure" },
+    { "title": "The Code Revolution", "author": "Ada Sparks", "year": 2021, "genre": "Technology" },
+    { "title": "Quiet Skies", "author": "Lana Brooks", "year": 2015, "genre": "Fiction" }
+];
